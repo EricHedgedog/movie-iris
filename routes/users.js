2aa858c112295c33a16ede5b53ebab9a17ad7882
mayuehua <mayuehua@kedacom.com>
2018-06-10 23:03:32 +0800

    register api

--- routes/users.js
+++ routes/users.js
@@ -37,4 +37,29 @@ router.post('/auth', function(req, res, next) {
 });
 })
 
+router.post('register', function(req, res, next) {
+  User.find({
+    username: req.body.username
+  },(err, data)=>{
+    if(err){
+        res.send('server or db error');
+    }else{
+       console.log(req.body)
+        if (!req.body.username || !req.body.password){
+          res.json({ status: 200, message: '注册失败，用户名和密码不能为空' });
+        } else if(data.length !== 0){
+            res.json({ status: 200, message: '注册失败，用户名已存在' });
+        }else{
+          User.create({username:req.body.username, password:req.body.password}, function(error){
+            if(error) {
+                console.log(error);
+            } else {
+                console.log('save ok');
+                res.json({ status: 200, message: '注册成功' });
+            }
+        });
+        }
+    }
+});
+})
 module.exports = router;
--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -4,6 +4,6 @@ var Schema = mongoose.Schema;
 var userSchema = new Schema({
     username: String,
     password: String,
-    isadmin: Boolean
+    isadmin: { type:Boolean, default: false}
 });
 module.exports = userSchema;
